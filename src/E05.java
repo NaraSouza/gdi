5ea4717a5d18a235964bd3a036a8c39d59942e6e
Nara <nsa2@cin.ufpe.br>
2018-05-22 22:39:57 -0300

    alterações nos comentários e em nomes de variáveis, adicionando mais informações sobre pessoa

--- src/E05.java
+++ src/E05.java
@@ -3,48 +3,66 @@ import java.io.File;
 import java.sql.*;
 
 public class E05 {
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "oracle.jdbc.driver.oracledriver";
-    static final String DB_URL = "jdbc:oracle:thin:@oracle11g.cin.ufpe.br:1521:Instance01";
+    // Driver JDBC e URL do banco de dados
+    static final String JDBC_DRIVER = "oracle.jdbc.OracleDriver";
+    static final String DB_URL = "jdbc:oracle:thin:@itapissuma.cin.ufpe.br:1521:dbdisc";
 
-    //  Database credentials
+    // Credenciais da equipe
     static final String USER = "g181if685cc_eq01";
-    static final String PASS = "qkcmjnwz";
+    static final String PASSWORD = "qkcmjnwz";
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
+        Connection connection = null;
+        Statement statement = null;
 
         try {
-            //STEP 2: Register JDBC driver
+            //Registrando o driver
             Class.forName(JDBC_DRIVER);
 
-            //STEP 3: Open a connection
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //Abrindo a conexão
+            connection = DriverManager.getConnection(DB_URL,USER, PASSWORD);
 
-            //STEP 4: Execute a query
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
+            //Executando a query
+            statement = connection.createStatement();
             File foto_pessoa = new File("/home/CIN/nsa2/IdeaProjects/gdi/src/foto.png");
-            Date data_nascimento = new Date(06/06/1996);
-            sql = "INSERT INTO PESSOA VALUES('333.333.333-33', 'Danila Freitas', 'M', data_nascimento, 'Rua Aquarela', '420', 'Arco-íris', 'Colorida', 'Pernambuco', 'danila.freitas@email.com', foto_pessoa); SELECT nome_pessoa, foto_pessoa FROM PESSOA;";
-            ResultSet rs = stmt.executeQuery(sql);
+            Date data_nascimento_pessoa = new Date(06/06/1996);
+            String sql = "INSERT INTO PESSOA VALUES('333.333.333-33', 'Danila Freitas', 'M', data_nascimento_pessoa," +
+                    " 'Rua Aquarela', '420', 'Arco-íris', 'Colorida', 'Pernambuco', 'danila.freitas@email.com', " +
+                    "foto_pessoa); SELECT * FROM PESSOA;";
+            ResultSet rs = statement.executeQuery(sql);
 
-            //STEP 5: Extract data from result set
+            //Extraindo dados do ResultSet
             while(rs.next()){
                 //Retrieve by column name
                 String nome  = rs.getString("nome_pessoa");
                 Blob foto = rs.getBlob("foto_pessoa");
+                String cpf = rs.getString("cpf_pessoa");
+                String sexo = rs.getString("sexo_pessoa");
+                Date data_nascimento = rs.getDate("data_nascimento__pessoa");
+                String email = rs.getString("email_pessoa");
+                String rua = rs.getString("rua_pessoa");
+                int numero = rs.getInt("numero_pessoa");
+                String bairro = rs.getString("bairro_pessoa");
+                String cidade = rs.getString("cidade_pessoa");
+                String estado = rs.getString("estado_pessoa");
 
                 //Display values
                 System.out.print("Nome: " + nome);
                 System.out.print(", Foto: " + foto);
+                System.out.print(", CPF: " + cpf);
+                System.out.print(", Sexo: " + sexo);
+                System.out.print(", Data de nascimento: " + data_nascimento);
+                System.out.print(", E-mail: " + email);
+                System.out.print(", Rua: " + rua);
+                System.out.print(", Número: " + numero);
+                System.out.print(", Bairro: " + bairro);
+                System.out.print(", Cidade: " + cidade);
+                System.out.print(", Estado: " + estado);
             }
-            //STEP 6: Clean-up environment
+            //Fechando ResultSet, Statement e Conexão
             rs.close();
-            stmt.close();
-            conn.close();
+            statement.close();
+            connection.close();
         }catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();
@@ -52,18 +70,17 @@ public class E05 {
             //Handle errors for Class.forName
             e.printStackTrace();
         }finally{
-            //finally block used to close resources
             try{
-                if(stmt!=null)
-                    stmt.close();
+                if(statement!=null)
+                    statement.close();
             }catch(SQLException se2){
-            }// nothing we can do
+            }
             try{
-                if(conn!=null)
-                    conn.close();
+                if(connection!=null)
+                    connection.close();
             }catch(SQLException se){
                 se.printStackTrace();
-            }//end finally try
-        }//end try
-    }//end main
-}//end FirstExample
+            }
+        }
+    }
+}
